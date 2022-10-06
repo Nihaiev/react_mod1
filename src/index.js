ca9a9799cff9dd9f65a259407f3d24ddc503cc57
Ivan <nigaev.ivan@gmail.com>
2022-10-06 21:34:41 +0300

    ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const element = React.createElement("div", { a: 5, b: 10 }, "Hello world");
+/**
+ * створюємо елемент і передаємо пропси
+ * - пропси - це налаштування елементу ('div', { a: 5, b: 10 }, 'Hello world');
+ * - children - це діти нашого елементу, те що в результаті в нього буде вкладено
+ *
+ *  */
+
+const elem1 = React.createElement('span', { children: 'Hello' });
+const elem2 = React.createElement('span', { children: 'world' });
+const element = React.createElement('div', {
+  a: 5,
+  b: 10,
+  children: [elem1, ' ', elem2], // children це властивість нашого елементу в якому можуть бути інші елементи
+});
 console.log(element);
+
+//рендеримо елементи, за це відповідає ReactDOM -рендерить наші React елементи у справжнє DOM дерево
+ReactDOM.render(element, document.querySelector('#root'));
 
 //===================================================================
 // import React from 'react';
