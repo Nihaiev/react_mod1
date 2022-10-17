d6d0bf537ccb722691ba346285822c05917cacc2
Ivan <nigaev.ivan@gmail.com>
2022-10-17 21:54:19 +0300

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import PaintingList from './components/PaintingList';
 import Section from './components/Section';
 import paintings from './paintings.json';
+import styled from 'styled-components';
+
+const Text = styled.p`
+  color: red;
+`;
+
 export default function App() {
   return (
     <div>
+      <Text>Themes</Text>
       <Section title="Топ тижня">
         <PaintingList items={paintings} />
       </Section>
